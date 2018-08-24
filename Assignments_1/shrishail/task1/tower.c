bf8170e85156d038eeb78ff7bfbbea20b59415bd
shrishail78 <shrishail@globaledgesoft.com>
2018-08-24 11:15:33 +0530

    Modified tower.c file

--- Assignments_1/shrishail/task1/tower.c
+++ Assignments_1/shrishail/task1/tower.c
@@ -7,6 +7,16 @@ Date:23-8-18
 Last modified:23-8-18
 */
 
+/*
+ * Header Guard
+ */
+#ifndef _HEADER_H
+
+/*
+ * Header Guard
+ */
+#define _HEADER_H
+
 /**
  *@headerfile stdio.h
  */
@@ -40,25 +50,29 @@ int main(void)
     printf("enter the number of disks\n");
     fgets(str, MAX, stdin);
     disk = atoi(str);
-    tower_of_honoi(disk, 'A', 'C', 'B'); ///function call for tower of honoi\
-    to send disks from source to destination
-    free(str);
+    tower_of_honoi(disk, 'A', 'C', 'B'); ///function call for tower of \
+    honoi which takes argumnets disks  A as a source tower B as \
+        destination tower C as a auxilary tower
+        free(str);
     str=NULL;
     return 0;
+#endif ///end of headerguard
 }
 void tower_of_honoi(int disk1,
         char source,
         char destination,
         char auxilary)
 {
-    if (disk1 == 1) {///if disk is only one move disk from source to destination
-        printf("disk 1 moves from %c to %c\n", source, destination);
+    if (disk1 == 1) {///if disk is only one move disk \
+        from source to destination
+            printf("disk 1 moves from %c to %c\n", source, destination);
         return;
     }
     else { ///recursive call for disks to move from source to auxilary rod
         tower_of_honoi(disk1-1,source, auxilary, destination);
-        printf("disk %d  moves from %c to %c\n", disk1, source, destination);
-        tower_of_honoi(disk1-1,auxilary, destination, source); ///recursive \
-        call for disks to move from auxilary to destination rod
+        printf("disk%d moves from %c to %c\n", disk1, source, destination);
+        tower_of_honoi(disk1-1,auxilary, destination, source);///recursive\
+        call for disks to move from auxilary to destination 
+
     }
 }///end for tower of honoi function
