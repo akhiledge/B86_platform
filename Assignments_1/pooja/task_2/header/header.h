fbd3e2883e3dfd90040534c431e1a6c4274ff8ae
Poojashree-M <m.poojashree@globaledgesoft.com>
2018-09-07 16:54:44 +0530

    Hash_table

--- Assignments_1/pooja/task_2/header/header.h
+++ Assignments_1/pooja/task_2/header/header.h
@@ -1,6 +1,12 @@
-///Header files inclusion
+/**
+ * File              : task_2/header/header.h
+ * Author            : Poojashree M <m.poojashreeglobaledgesoft.com>
+ * Date              : 23.07.2018
+ * Last Modified Date: 02.09.2018
+ * Last Modified By  : Poojashree M <m.poojashreeglobaledgesoft.com>
+ */
 
-///Header guard
+/**Header guard*/
 #ifndef HEADER_H
 #define HEADER_H
 
@@ -11,19 +17,20 @@
 #include<assert.h>
 
 #define MAX 10						/**@def Macro for number of files*/
-#define SIZE 200						/**@def Size of string*/
-#define ARR_SIZE 50						/**@def Size of hashtable*/
-///structure declaration
+#define SIZE 200					/**@def Size of string*/
+#define ARR_SIZE 50					/**@def Size of hashtable*/
+/**structure declaration*/
 struct node{
-	char *data;						///structure member for data of type character ptr
-	int count[MAX];						///structure member for count
+	char *data;						/**structure member for data of
+									  type character ptr*/
+	int count[MAX];					/**structure member for count*/
 	char filename[MAX][SIZE]; 
-	struct node *next;				///structure member for next node
+	struct node *next;				/**structure member for next node*/
 };
 typedef struct node NODE;		
 
 enum choice {INSERT = 1, COPY, SEARCH, EXIT};
-///function declarations
+/**function declarations*/
 void insert(char *, char *, int);
 int hash_func(char);
 NODE *create_node(char *str);
@@ -34,5 +41,5 @@ void display_node(NODE *);
 void modify(char *);
 
 extern NODE *hashtable[ARR_SIZE]; 
-///End of header guard
+/**End of header guard*/
 #endif
