4b17cf76a61c457b1b0b83188cd13a7483fbe1d5
Poojashree-M <m.poojashree@globaledgesoft.com>
2018-09-07 16:29:36 +0530

    Hash table

--- Assignments_1/pooja/task_2/source/hash_tab_insert.c
+++ Assignments_1/pooja/task_2/source/hash_tab_insert.c
@@ -1,58 +1,78 @@
-/**Function to insert file contents in Hash table*/
+/**
+ * File              : hash_tab_insert.c
+ * Author            : Poojashree M <m.poojashreeglobaledgesoft.com>
+ * Date              : 0.0.2018
+ * Last Modified Date: 02.09.2018
+ * Last Modified By  : Poojashree M <m.poojashreeglobaledgesoft.com>
+ */
+
+/**@brief: File contain function to insert file contents in
+*					Hash table
+*@param: str is the word to be inserted in hash table
+*		file is the name of the file
+*		argc is number of files
+*@return: void*/
+
 void insert(char *str, char *file, int argc)
 {
-	struct node *head = NULL;		/*!<struct node pointer used for traversing linked list*/
+	struct node *head = NULL;		/*!<struct node pointer used for
+									  traversing linked list*/
 	struct node *new = NULL;		/*!struct node ptr for new node*/
 	struct node *prev = NULL;
 	int index = 0;					/*!index of hashtable*/
 	int i = 0;
 
-	index = hash_func(*str);		///call hash function
-	if(hashtable[index] == NULL){	///initial condition
-		new = create_node(str); ///creation of new node
-		hashtable[index] = new;		///enter the new node address to hash table, which is head node
-		(new->count[0])++;					///increment count to 1
+	index = hash_func(*str);		/**call hash function*/
+	if (hashtable[index] == NULL) {	/**initial condition*/
+		new = create_node(str);		/**creation of new node*/
+		hashtable[index] = new;		/**enter the new node address to
+									  hash table, which is head node*/
+		(new->count[0])++;			/**increment count to 1*/
 		strcpy(new->filename[0], file);
 		return;
-	}
-	else{							///
-		head = hashtable[index];	///ptr entry in hashtable is head of linked list
-		prev = NULL;				///previous node
-		while(head != NULL){
-			if(strcasecmp(str, head->data) == 0){ ///if the head node data equal to the new data
-				for(i = 0; i < (argc - 2); i++){ 
-					if(strcmp(head->filename[i],file) == 0)
-						(head->count[i])++;					///increment count
-					else{
-						strcpy(head->filename[i+1],file);
+	} else {
+		head = hashtable[index];	/**ptr entry in hashtable is
+									  head of linked list*/
+		prev = NULL;				/**previous node*/
+		while (head != NULL) {
+			if (strcasecmp(str, head->data) == 0) { /**if the head node data
+													  equal to the new data*/
+				for (i = 0; i < (argc - 2); i++) { 
+					if (strcmp(head->filename[i],file) == 0) {
+						(head->count[i])++;			/**increment count*/
+					}
+					else {
+						strcpy(head->filename[i+1], file);
 						(head->count[i+1])++;
 					}
 					return;
 				}
 			}
 
-			if(strcasecmp(str,head->data) < 0){	///if new data is smaller than the existing node
-				new = create_node(str);				///sort the nodes based on data
+			if (strcasecmp(str,head->data) < 0) {	/**if new data is smaller
+													than the existing node*/
+				new = create_node(str);				/**sort the nodes
+													  based on data*/
 				strcpy(new->filename[0], file);
-				if(head == hashtable[index]){
+				if (head == hashtable[index]) {
 
 					hashtable[index] = new;
 					new->next = head;
 					(new->count[0])++;
-				}
-				else{
+				} else {
 					new->next = prev->next;
 					prev->next = new;
 					(new->count[0])++;
-
 				}
 				return;
 			}
-			prev = head;					///if not the previous conditions, traverse the list
+			prev = head;					/**if not the previous
+											conditions, traverse the list*/
 			head = head->next;
 		}
 
-		if(head == NULL){						///if the new node has to be inserted at the end of list
+		if (head == NULL) {					/**if the new node has to be
+											inserted at the end of list*/
 			new = create_node(str);
 			strcpy(new->filename[0],file);
 			new->next = prev->next;
--- Assignments_1/pooja/task_2/source/search_content.c
+++ Assignments_1/pooja/task_2/source/search_content.c
@@ -1,25 +1,39 @@
-/**@brief Function definition to search the particular content and return
-*@param str contains the input string from user
-*@return 0 on success, -1 on failure*/
+/**
+ * File              : search_content.c
+ * Author            : Poojashree M <m.poojashreeglobaledgesoft.com>
+ * Date              : 24.07.2018
+ * Last Modified Date: 02.09.2018
+ * Last Modified By  : Poojashree M <m.poojashreeglobaledgesoft.com>
+ */
+
+/**@brief: Function definition to search the particular content and return
+*@param: str contains the input string from user
+*@return: 0 on success, -1 on failure*/
 int search(char *str)
 {
-	NODE *traverse = NULL;	/*!<struct node ptr to traverse list*/
-	int index = 0;			/*!<variable for hashtable index*/
+	NODE *traverse = NULL;							/**struct node ptr to
+													  traverse list*/
+	int index = 0;									/**variable for hashtable
+													  index*/
 	char *msg = NULL;
 	
-	index = hash_func(*str);	///calling hash function
-	if(hashtable[index] == NULL){	///if no entry present
+	index = hash_func(*str);						/**calling hash function*/
+	if (hashtable[index] == NULL) {						/**if no entry present*/
 		return -1;
-	}
-	else{
-		traverse = hashtable[index];			///if entry present, traverse through list
-		while(traverse != NULL){
-			if(strcasecmp(traverse->data, str) == 0){	///if string matches print details about node
+	} else {
+		traverse = hashtable[index];					/**if entry present,
+														traverse through list*/
+		while (traverse != NULL) {
+			if (strcasecmp(traverse->data, str) == 0) {	/**if string matches
+														print details about
+														node*/
 				display_node(traverse);
-				return 0;								///returns 0 0n success
+				return 0;								/**returns 0 on
+														 success*/
 			}
 			traverse = traverse->next;
 		}
-			return -1;									///returns -1 if not found
+			return -1;									/**returns -1 if not
+														found*/
 	}
 }
